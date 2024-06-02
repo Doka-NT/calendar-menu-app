2308252ae75880c99feb18b9c00270989c73989e
Artem Soshnikov <213036@skobka.com>
2024-06-02 17:19:01 +0500

    Initial Commit

---
+++ CalendarMenuApp/CalendarMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarMenuAppApp.swift
+//  CalendarMenuApp
+//
+//  Created by Артем Сошников on 02.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
