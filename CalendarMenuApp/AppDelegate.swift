83b06e00c1db8805b762eaecd5904853ab5de8c5
Artem Soshnikov <213036@skobka.com>
2024-06-02 17:54:31 +0500

    Рабочий прототип приложения

---
+++ CalendarMenuApp/AppDelegate.swift
@@ -0,0 +1,111 @@
+//
+//  AppDelegate.swift
+//  CalendarMenuApp
+//
+//  Created by Артем Сошников on 02.06.2024.
+//
+
+import Foundation
+import AppKit
+import EventKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Создаем элемент строки меню
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "calendar", accessibilityDescription: "Calendar")
+            button.action = #selector(showMenu)
+        }
+        
+        let eventStore = EKEventStore()
+
+        eventStore.requestAccess(to: .event) { (granted, error) in
+            if granted {
+                // Доступ предоставлен
+            } else {
+                // Доступ не предоставлен
+                print("Access to calendar was not granted: \(String(describing: error?.localizedDescription))")
+            }
+        }
+
+    }
+    
+    @objc func showMenu() {
+        let menu = NSMenu()
+        
+        // Загружаем события из календаря
+        let events = loadEventsForToday()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm" // Формат времени
+        
+        for event in events {
+            // Проверяем, занимает ли событие весь день
+            if !event.isAllDay {
+                // Получаем время начала события
+                if let startTime = event.startDate {
+                    let startTimeString = dateFormatter.string(from: startTime)
+                    
+                    // Формируем строку вида "Время начала - Название события"
+                    var eventTitle = "\(startTimeString)"
+                    if let title = event.title {
+                        eventTitle += " - \(title)"
+                    }
+                    
+                    let menuItem = NSMenuItem(title: eventTitle, action: #selector(openEvent(_:)), keyEquivalent: "")
+                    menuItem.representedObject = event
+                    menu.addItem(menuItem)
+                }
+            }
+        }
+        
+        // Показать меню
+        statusItem.menu = menu
+        statusItem.button?.performClick(nil) // Показать меню при нажатии на иконку
+        statusItem.menu = nil // Сбросить меню после показа
+    }
+
+
+
+    
+    func loadEventsForToday() -> [EKEvent] {
+        let eventStore = EKEventStore()
+        let calendars = eventStore.calendars(for: .event)
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let endOfDay = Calendar.current.date(byAdding: .day, value: 1, to: startOfDay)!
+        
+        let predicate = eventStore.predicateForEvents(withStart: startOfDay, end: endOfDay, calendars: calendars)
+        let events = eventStore.events(matching: predicate)
+        return events
+    }
+    
+    @objc func openEvent(_ sender: NSMenuItem) {
+        if let event = sender.representedObject as? EKEvent {
+            let description = event.notes ?? ""
+            if let url = extractURL(from: description) {
+                print(url)
+                NSWorkspace.shared.open(url)
+            } else {
+                // Открыть событие в календаре
+                if let eventURL = URL(string: "ical://ekevent/\(event.eventIdentifier)") {
+                    NSWorkspace.shared.open(eventURL)
+                }
+            }
+        }
+    }
+
+    func extractURL(from text: String) -> URL? {
+        let patterns = ["https://telemost.yandex.ru", "https://salutejazz.ru", "https://jazz.sber.ru"]
+        for pattern in patterns {
+            if let range = text.range(of: pattern) {
+                let urlString = text[range.lowerBound...].components(separatedBy: .whitespacesAndNewlines).first ?? ""
+                return URL(string: urlString)
+            }
+        }
+        return nil
+    }
+
+}
--- CalendarMenuApp/CalendarMenuApp.swift
+++ CalendarMenuApp/CalendarMenuApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import AppKit
+import EventKit
 
 @main
-struct CalendarMenuAppApp: App {
+struct CalendarMenuApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
